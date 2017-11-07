0712e1916087f5ac9e2097ade2ef3c44ba3a2fba
Hugo Leonel <hugoleone.net@gmail.com>
2017-11-07 13:02:40 -0200

    Dois executáveis para Teste

--- TI/app.js
+++ TI/app.js
@@ -11,70 +11,27 @@ var rotaLogin = require('./app/routes/login');
 rotaLogin(app);
 */
 
-/*
 
+//CASO QUEIRAM TESTAR CONEXÃO COM BANCO E MODULARIDADE: Digitem no terminal: "node appTesteBD.js"
 
 app.listen(3000, function(){
 	console.log("Servidor funfando");
 	
 	console.log("!+++++++++++++++++++++++++++++++++++++++++!");
 	console.log("!TESTE TESTE TESTE TESTE TESTE TESTE TESTE!");
+	console.log("!SITE	SITE	SITE	SITE	SITE	SITE!");
 	console.log("!TESTE TESTE TESTE TESTE TESTE TESTE TESTE!");
+	console.log("!SITE	SITE	SITE	SITE	SITE	SITE!");
 	console.log("!TESTE TESTE TESTE TESTE TESTE TESTE TESTE!");
+	console.log("!SITE	SITE	SITE	SITE	SITE	SITE!");
 	console.log("!TESTE TESTE TESTE TESTE TESTE TESTE TESTE!");
+	console.log("!SITE	SITE	SITE	SITE	SITE	SITE!");
 	console.log("!=========================================!");
-	console.log(app);
 
 });
 
-*/
 
 
 
-//DESCOMENTEM CASO QUEIRAM TESTAR CONEXÕES E MODULO DE DAO
-//COMENTEM O app.listen({ ... }); 
 
-console.log("========================================================================================================");
-console.log("APP");
-console.log("APP: " + app);
-console.log("========================================================================================================");
-console.log("APP.APP");
-console.log(app.app);
-console.log("========================================================================================================");
-console.log("APP.CONFIG");
-console.log(app.config);
-console.log("========================================================================================================");
-console.log("APP.CONFIG.DBCONNECTION");
-console.log(app.config.dbConnection);
-console.log("========================================================================================================");
-console.log("APP.CONFIG.DBCONNECTION.INSTANCE_CONN");
-console.log(app.config.dbConnection.instance_conn);
-console.log("========================================================================================================");
-console.log("APP.CONFIG.DBCONNECTION.INSTANCE_CONN EXEC");
-console.log(app.config.dbConnection.instance_conn());
-console.log("========================================================================================================");
-console.log("APP.APP.MODELS");
-console.log(app.app.models);
-console.log("========================================================================================================");
-console.log("APP.APP.MODELS.TESTEDAO");
-console.log(app.app.models.TesteDAO);
-console.log("========================================================================================================");
-console.log("APP.APP.MODELS.TESTEDAO EXEC");
-var instance = app.config.dbConnection.instance_conn();
-console.log(app.app.models.TesteDAO(instance));
-console.log("========================================================================================================");
-console.log("APP.APP.MODELS.TESTEDAO NEW EXEC");
-console.log(new app.app.models.TesteDAO(instance));
-console.log("========================================================================================================");
-console.log("MODEL = NEW EXEC");
-var model = new app.app.models.TesteDAO(instance);
-console.log(model);
-console.log("========================================================================================================");
-console.log("MODEL.GETTESTE");
-console.log(model.getTeste);
-console.log("========================================================================================================");
-console.log("MODEL.GETTESTE EXEC");
-
-console.log(" CONSOLE.LOG(model.getTeste()): " + model.getTeste());
-console.log("--------------------------------------------------------------------------------------------------------");
-
+ 
